29eff2e0e997fcfd5aef3348c456ad43e075a351
chi chan <chitsuenchan@gmail.com>
2023-05-16 15:47:21 +0800

    added a list object of starters

--- app/src/main/java/com/example/thehungerdeveloper/Dish.java
+++ app/src/main/java/com/example/thehungerdeveloper/Dish.java
@@ -1,13 +1,22 @@
 package com.example.thehungerdeveloper;
 
+import androidx.annotation.NonNull;
+
 public class Dish {
 
     String title;
     String description;
     int price;
 
-    Dish() {
-
+    Dish(String title, String description, int price) {
+        this.title = title;
+        this.description = description;
+        this.price = price;
     }
 
+    @NonNull
+    @Override // We want to customize this method not the default behaviour so this is why we have Override
+    public String toString() {
+        return title;
+    }
 }
--- app/src/main/java/com/example/thehungerdeveloper/StartersActivity.java
+++ app/src/main/java/com/example/thehungerdeveloper/StartersActivity.java
@@ -15,37 +15,20 @@ public class StartersActivity extends AppCompatActivity {
 
         ListView startersList = findViewById(R.id.list_view_starters);
 
-        Dish dishOne = new Dish();
-
-        dishOne.title = "Grilled Salmon with Lemon Butter Sauce";
-        dishOne.description = "Great salmon food with extra love";
-        dishOne.price = 9;
-
-
-        String[] dishes = {
-                "Grilled Salmon with Lemon Butter Sauce",
-                "Chicken Parmesan with Spaghetti",
-                "Beef Tenderloin Medallions with Red Wine Reduction",
-                "Shrimp Scampi Pasta",
-                "Vegetable Stir-Fry with Tofu",
-                "Lobster Ravioli in Creamy Tomato Sauce",
-                "BBQ Pulled Pork Sandwich with Coleslaw",
-                "Margherita Pizza with Fresh Basil",
-                "Chicken Tikka Masala with Basmati Rice",
-                "Eggplant Parmesan with Marinara Sauce",
-                "Beef Bulgogi with Steamed Rice",
-                "Seafood Paella with Saffron Rice",
-                "Spinach and Ricotta Stuffed Chicken Breast",
-                "Veggie Pad Thai with Peanut Sauce",
-                "Lamb Kebabs with Mint Yogurt Sauce",
-                "Mushroom Risotto with Truffle Oil",
-                "Teriyaki Glazed Salmon with Jasmine Rice",
-                "Chicken and Waffles with Maple Syrup",
-                "Spaghetti Carbonara with Pancetta and Parmesan",
-                "Sushi Platter with a Variety of Rolls and Sashimi"
+        Dish[] dishes = {
+                new Dish("Grilled Salmon with Lemon Butter Sauce", "Great salmon food with extra love", 9 ),
+                new Dish("Chicken Parmesan with Spaghetti", "Chicken made with even more love", 5 ),
+                new Dish("Beef Tenderloin with Red Wine Reduction", "Tender beef cooked to perfection", 12),
+                new Dish("Vegetable Curry with Rice", "A flavorful blend of vegetables and spices", 8),
+                new Dish("Shrimp Scampi with Linguine", "Juicy shrimp tossed in a garlic-infused butter sauce", 10),
+                new Dish("Mushroom Risotto with Truffle Oil", "Creamy risotto with earthy mushroom flavors", 7),
+                new Dish("BBQ Ribs with Coleslaw and Cornbread", "Fall-off-the-bone ribs with classic sides", 14),
+                new Dish("Spinach and Feta Stuffed Chicken Breast", "Savory chicken breast filled with spinach and feta", 6),
+                new Dish("Lemon Herb Roasted Chicken with Roasted Potatoes", "Tender chicken infused with zesty flavors", 9),
+                new Dish("Pasta Primavera with Garlic Bread", "A medley of fresh vegetables served over pasta", 8)
         };
 
-        ArrayAdapter<String> dishesAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, dishes);
+        ArrayAdapter<Dish> dishesAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, dishes);
 
         startersList.setAdapter(dishesAdapter);
 
